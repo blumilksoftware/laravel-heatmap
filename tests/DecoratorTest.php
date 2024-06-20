a92abd0b7947e693282f710d2f35862eb3875744
BarTracz <bartosztracz68@gmail.com>
2024-06-20 18:55:55 +0200

    tests

---
+++ tests/DecoratorTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+use Blumilk\LaravelHeatmap\Decorators\TailwindDecorator;
+use Blumilk\LaravelHeatmap\HeatmapBuilder;
+use Blumilk\LaravelHeatmap\Tile;
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
+
+class DecoratorTest extends TestCase
+{
+    public function testBasicArrayAccessItems(): void
+    {
+        $data = [
+            ["created_at" => "2022-11-13 00:00:00"],
+            ["created_at" => "2022-11-13 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-18 00:00:00"],
+        ];
+
+        $builder = new HeatmapBuilder(
+            now: Carbon::parse("2022-11-18"),
+            decorator: new TailwindDecorator("green"),
+            timezone: new \Carbon\CarbonTimeZone('1')
+        );
+
+        $result = $builder->build($data);
+
+        $this->assertSame(
+            expected: [0, 0, 2, 5, 0, 7, 0, 1],
+            actual: array_map(fn(Tile $item): int => $item->count, $result),
+        );
+
+        $defaultClasses = "size-4 border border-gray-300 rounded";
+
+        $this->assertSame(
+            expected: array_map(
+                fn(string $class): string => "$defaultClasses $class",
+                ["bg-white", "bg-white", "bg-green-100", "bg-green-600", "bg-white", "bg-green-900", "bg-white", "bg-green-50"],
+            ),
+            actual: array_map(fn(Tile $item): string => $item->description, $result),
+        );
+    }
+}
---
+++ tests/PeriodIntervalTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Blumilk\LaravelHeatmap\HeatmapBuilder;
+use Blumilk\LaravelHeatmap\PeriodInterval;
+use Blumilk\LaravelHeatmap\Tile;
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
+
+class PeriodIntervalTest extends TestCase
+{
+    public function testChangingPeriodForMonth(): void
+    {
+        $builder = new HeatmapBuilder(
+            now: Carbon::parse("2022-11-19"),
+            periodInterval: PeriodInterval::Monthly,
+            timezone: new \Carbon\CarbonTimeZone('1')
+        );
+        $builder->forLastYear();
+
+        $result = $builder->build($this->getData());
+
+        $this->assertSame(
+            expected: [0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 1, 0, 1],
+            actual: array_map(fn(Tile $item): int => $item->count, $result),
+        );
+    }
+
+    protected function getData(): array
+    {
+        return [
+            ["created_at" => "2021-01-01 00:00:00"],
+            ["created_at" => "2022-01-01 00:00:00"],
+            ["created_at" => "2022-04-16 00:00:00"],
+            ["created_at" => "2022-04-16 00:00:00"],
+            ["created_at" => "2022-09-18 00:00:00"],
+            ["created_at" => "2022-11-19 00:00:00"],
+        ];
+    }
+}

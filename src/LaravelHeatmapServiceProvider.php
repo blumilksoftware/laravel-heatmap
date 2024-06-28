567466b53fbb251b2833aed694290d762a610a39
BarTracz <115506125+BarTracz@users.noreply.github.com>
2024-06-28 11:29:34 +0200

    #1 - setup package skeleton (#6)

---
+++ src/LaravelHeatmapServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\LaravelHeatmap;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelHeatmapServiceProvider extends ServiceProvider
+{
+}

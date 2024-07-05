b323ae6b2bc24b0459107d4645acd975b5ea6d8a
BarTracz <115506125+BarTracz@users.noreply.github.com>
2024-07-05 12:00:28 +0200

    #2 - eloquent helper (#7)

---
+++ src/LaravelHeatmapBuilder.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Blumilk\LaravelHeatmap;
+
+use ArrayAccess;
+use Blumilk\Heatmap\Contracts\TimeGroupable;
+use Blumilk\Heatmap\HeatmapBuilder;
+use Blumilk\Heatmap\PeriodInterval;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use InvalidArgumentException;
+
+class LaravelHeatmapBuilder extends HeatmapBuilder
+{
+    public function buildFromCollection(Collection $data, string $arrayAccessIndex = self::DEFAULT_ARRAY_ACCESS_INDEX): array
+    {
+        $this->arrayAccessIndex = $arrayAccessIndex;
+
+        return parent::build($data);
+    }
+
+    public function buildFromArray(array $data, string $arrayAccessIndex = self::DEFAULT_ARRAY_ACCESS_INDEX): array
+    {
+        $this->arrayAccessIndex = $arrayAccessIndex;
+        $collection = collect($data);
+
+        return parent::build($collection);
+    }
+
+    /**
+     * @param iterable<array|ArrayAccess|TimeGroupable|Model> $data
+     */
+    protected function mapDataToCarbonElements(iterable $data): array
+    {
+        return array_map(
+            callback: fn(array|ArrayAccess|TimeGroupable|Model $item): Carbon => $this->mapToCarbon($item),
+            array: [...$data],
+        );
+    }
+
+    protected function mapToCarbon(array|ArrayAccess|TimeGroupable|Model $item): Carbon
+    {
+        if ($item instanceof TimeGroupable) {
+            $date = Carbon::parse($item->getTimeGroupableIndicator());
+        } elseif ($item instanceof Model) {
+            if (!isset($item->{$this->arrayAccessIndex})) {
+                throw new InvalidArgumentException("The property '{$this->arrayAccessIndex}' does not exist on the given model.");
+            }
+            $date = Carbon::parse($item->{$this->arrayAccessIndex});
+        } else {
+            if (!isset($item[$this->arrayAccessIndex])) {
+                throw new InvalidArgumentException("The key '{$this->arrayAccessIndex}' does not exist in the given array.");
+            }
+            $date = Carbon::parse($item[$this->arrayAccessIndex]);
+        }
+
+        $date->setTimezone($this->timezone);
+
+        return match ($this->periodInterval) {
+            PeriodInterval::Monthly => $date->startOfMonth(),
+            PeriodInterval::Weekly => $date->startOfWeek(),
+            PeriodInterval::Daily => $date->startOfDay(),
+            PeriodInterval::Hourly => $date->startOfHour(),
+        };
+    }
+}
